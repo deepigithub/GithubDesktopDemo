53b3723f3504cc618ba021db0ba9e7449d2df1b6
vmvinothkumar <vmvinothkumar@gmail.com>
2015-08-13 16:48:42 +0530

    Initial Commit

---
+++ MainProject/MainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MainProject
+//
+//  Created by vmvinothkumar on 13/08/15.
+//  Copyright (c) 2015 vmvinothkumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MainProject/MainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProject
+//
+//  Created by vmvinothkumar on 13/08/15.
+//  Copyright (c) 2015 vmvinothkumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
